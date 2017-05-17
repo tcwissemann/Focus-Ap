56ae6391f66113b47bb06381b6d32c7a1496e4a7
Thomas Wissemann <tcwissemann@gmail.com>
2017-05-16 22:58:18 -0500

    Update popup.js

--- 0.1/popup.js
+++ 0.1/popup.js
@@ -1,1 +1,7 @@
-// Stopwatch code
+var timer = setInterval(counter, 1000);
+var secs = 0;
+function counter() {
+  ++secs;
+  var seconds = secs;
+  document.getElementById("timer").innerHTML = secs;
+}
