9471edde0c510762fa781e196dd0015eb76ba703
Thomas Wissemann <tcwissemann@gmail.com>
2017-05-22 13:45:44 -0500

    Update popup.js

--- 0.1/popup.js
+++ 0.1/popup.js
@@ -1,21 +1,27 @@
-var btn = document.getElementById("notify");
+var button = document.getElementById("notify");
 var clock = document.getElementById("timer");
-btn.onclick = startWork;
+button.onclick = startWork;
 
-function startWork() {
-  el = document.documentElement,
-  rfs = el.requestFullScreen || el.webkitRequestFullScreen || el.mozRequestFullScreen;
-  rfs.call(el);
-//^gets fullscreen
+function startWork(){
   var timer = setInterval(counter, 1000);
   var secs = 0;
-  function counter() {
+  //call counter every second then start the timer at 0
+  function counter(){
     ++secs;
     var seconds = secs;
     clock.innerHTML = secs;
-    while(secs == 1800) {
+    //add 1 every second then replace the html element
+    while(secs == 1800){
       alert('Focus!');
       secs = 0;
+      //when the elements value is 1800 (thirty minutes) call an alert and reset
+    }
+    while(secs < 1800){
+      el = document.documentElement,
+      rfs = el.requestFullScreen || el.webkitRequestFullScreen || el.mozRequestFullScreen;
+      rfs.call(el);
+      //^ problem area - taken from stack overflow
+      //when the elements value is less than 1800 gets fullscreen
     }
   }
 }
